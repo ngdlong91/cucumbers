849309d76e314fe61fd99cd0472cce5deb3fe59b
ngdlong91 <long.nguyen@piksal.com>
2019-02-22 17:03:58 +0700

    - Update

--- dto/response.go
+++ dto/response.go
@@ -1,8 +1,21 @@
 package dto
 
+var DebugResponse bool
+
+func SetDebugResponse(val bool) {
+	DebugResponse = val
+}
+
 type Response struct {
 	StatusCode   int         `json:"status_code"`
 	Message      string      `json:"message"`
 	DebugMessage string      `json:"debug_message,omitempty"`
-	Data         interface{} `json:"data, omitempty"`
+	Data         interface{} `json:"data,omitempty"`
+}
+
+func (r *Response) SetDebugMsg(msg string) *Response {
+	if DebugResponse {
+		r.DebugMessage = msg
+	}
+	return r
 }
