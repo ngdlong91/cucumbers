570448da8f424b66999d343fcf83b5e73e90b20d
Thanh <1512510@student.hcmus.edu.vn>
2019-02-25 16:16:51 +0700

    add mysql connection

--- v1/dto/response.go
+++ v1/dto/response.go
@@ -3,5 +3,5 @@ package dto
 type Response struct {
 	StatusCode int         `json:"status_code"`
 	Message    string      `json:"message"`
-	Data       interface{} `json:"data, omitempty"`
+	Data       interface{} `json:"data,omitempty"`
 }
